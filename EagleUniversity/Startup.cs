bb5d4f381c3f5062cc9a3e8b24bd3c052fe04a4b
PES <entermosjava@gmail.com>
2018-03-06 09:20:45 +0100

    Add project files.

---
+++ EagleUniversity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EagleUniversity.Startup))]
+namespace EagleUniversity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
