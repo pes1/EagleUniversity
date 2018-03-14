7163d194eeb29ec00b2380dace5107a203b4f49c
Paul Bespalov <goyza666@gmail.com>
2018-03-14 16:41:16 +0100

    Assemble Course i.1

---
+++ EagleUniversity/Models/ViewModels/ActivityViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EagleUniversity.Models.ViewModels
+{
+    public class ActivityViewModel
+    {
+        public int Id { get; set; }
+        public string ActivityName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int ModuleId { get; set; }
+        public int ActivityTypeId { get; set; }
+    }
+}
