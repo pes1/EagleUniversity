7554fd764cb478273009ceb6bd8e38f6f25317c6
Evorn Mårtensson <evorn.martensson@outlook.com>
2018-03-16 10:19:34 +0100

    Change Datepick

--- EagleUniversity/Models/Activity.cs
+++ EagleUniversity/Models/Activity.cs
@@ -6,6 +6,8 @@ using System.Web;
 
 namespace EagleUniversity.Models
 {
+    //public enum Epass { FM = 1, EM, FMEM };
+
     public class Activity
     {
         public int Id { get; set; }
@@ -14,9 +16,10 @@ namespace EagleUniversity.Models
         public DateTime StartDate { get; set; }
         [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
+        //public Epass Pass { get; set; }
         public int ModuleId { get; set; }
-
         public int ActivityTypeId { get; set; }
+
         //Nav Prop
         public virtual ActivityType ActivityTypes { get; set; }
         public virtual Module Modules { get; set; }
