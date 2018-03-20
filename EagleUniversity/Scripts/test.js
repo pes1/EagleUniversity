92b87759d0c76a6ba6d2ef57d39c889e4fbb1f33
Paul Bespalov <goyza666@gmail.com>
2018-03-20 16:50:49 +0100

    users

--- EagleUniversity/Scripts/test.js
+++ EagleUniversity/Scripts/test.js
@@ -17,10 +17,21 @@ $(document).ready(
         {
             $("#Default").removeClass("active");
             $("#home").removeClass("in active");
+            $("#Student").removeClass("active");
+            $("#menu1").removeClass("in active");
             $("#Document").addClass("active");
             $("#menu2").addClass("in active");
         }
-        $("#Redirected").hide();
+        else if ($("#Redirected").text() === "Document") {
+            $("#Default").removeClass("active");
+            $("#home").removeClass("in active");
+            $("#Document").removeClass("active");
+            $("#menu2").removeClass("in active");
+            $("#Student").addClass("active");
+            $("#menu1").addClass("in active");
+        }
+
+        //$("#Redirected").hide();
   
     }
 );
