2028c8297955e65e334d2b571a48eb174b17cc74
User <User@DESK-FG2GJ32>
2018-03-20 16:38:32 +0100

    commit

--- EagleUniversity/Scripts/Collpase.js
+++ EagleUniversity/Scripts/Collpase.js
@@ -1,9 +1,21 @@
 ﻿$(function () {
-    $("#Demo").collapse('hide');   
+    $("tbody#firstlist").collapse('hide');
+    $("tfoot#secondlist").collapse('hide');
+});
+
+$(function () {
+    $("#Demo").collapse('hide');
 });
 $(function () {
-    $("#CollapseDetail").click(function(){
-        $("#Demo").collapse('toggle');
+    $("#CollapseDetail").click(function () {
+        $("#Demo").collapse('toggle');       
     });
-  
+});
+
+$(function () {
+    $(".btn.btn-link").click(function () {
+        var CurrentId = $(this).parent().parent().next().attr("id", "newId");
+        $(CurrentId).collapse('toggle');      
+
+    });
 });
