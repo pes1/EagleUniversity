6d2470256bc2802fa2b6d31518d0d7cc6a7e45e8
Paul Bespalov <goyza666@gmail.com>
2018-03-23 12:30:12 +0100

    Activity changes (AM/PM) Edit Delete Create

--- EagleUniversity/Models/ViewModels/ActivityViewModel.cs
+++ EagleUniversity/Models/ViewModels/ActivityViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,17 @@ namespace EagleUniversity.Models.ViewModels
     {
         public int Id { get; set; }
         public string ActivityName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
+        public bool StartDateAm { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set; }
+        public bool EndDateAm { get; set; }
         public int ModuleId { get; set; }
         public int ActivityTypeId { get; set; }
+        public commonEntity redirectProperty { get; set; }
+
     }
 }
---
+++ EagleUniversity/Models/commonEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EagleUniversity.Models
+{
+    public class commonEntity
+    {
+        public int Id { get; set; }
+        public string EntityType { get; set; }
+        public string returnController { get; set; }
+        public string returnMethod { get; set; }
+        public int returnId { get; set; }
+        public string returnTarget { get; set; }
+    }
+}
