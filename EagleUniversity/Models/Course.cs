982cc01935b92838faa8ad87fec604e08293db1b
pes1 <entermosjava@gmail.com>
2018-03-24 21:12:09 +0100

    Data Annotation checking in Course

--- EagleUniversity/Models/Course.cs
+++ EagleUniversity/Models/Course.cs
@@ -18,11 +18,13 @@ namespace EagleUniversity.Models
         [DataType(DataType.Date)]
         [Display(Name = "Start Date")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [StartDateTest]
         public DateTime StartDate { get; set; }
 
         [DataType(DataType.Date)]
         [Display(Name = "End Date")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [EndDateTest]
         public DateTime EndDate { get; set; }
 
         public string OwnerId { get; set; }
@@ -32,4 +34,73 @@ namespace EagleUniversity.Models
         public virtual ICollection<Assignments> UserCourseAssignments { get; set; }
         public virtual ICollection<CourseDocument> DocumentCourseAssignments { get; set; }
     }
-}
+
+
+    public class StartDateTestAttribute : ValidationAttribute
+    {
+        //private DateTime _dateTime;
+        //public StartDateTest(int Year) //- parameter in the annotation, if any.
+        //{                              //- like this: [StartDateTest(1)]
+        //    _dateTime = _DateTime;     //- need a cast to create a timespan.
+        //}
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = (Models.Course)validationContext.ObjectInstance;
+            //if (value != null) // lets check if we have some value
+            //{
+            //    if (value is DateTime) // check if it is a valid Date and Time object
+            //    {
+            DateTime _StartDate = Convert.ToDateTime(value);
+
+            if (_StartDate <= DateTime.Now)
+            {
+                return new ValidationResult("Start Date must be a future date.");
+            }
+            else { return ValidationResult.Success; }   //- The Date fulfilled the requirements.
+        }
+    }//- of class StartDateTestAttribute
+
+
+
+    public class EndDateTestAttribute : ValidationAttribute
+    {
+        //private DateTime _dateTime;
+        //public StartDateTest(int Year) //- parameter in the annotation, if any.
+        //{                              //- like this: [StartDateTest(1)]
+        //    _dateTime = _DateTime;     //- need a cast to create a timespan.
+        //}
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = (Models.Course)validationContext.ObjectInstance;
+            //if (value != null) // lets check if we have some value
+            //{
+            //    if (value is DateTime) // check if it is a valid Date and Time object
+            //    {
+            DateTime _EndDate = Convert.ToDateTime(value);
+
+            if (!(model.StartDate <= _EndDate))
+            {
+                return new ValidationResult("End Date must be a later than the start date.");
+            }
+            else { return ValidationResult.Success; }   //- The Date fulfilled the requirements.
+        }
+    }//- of class EndDateTestAttribute
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//- of namespace
